f864c0f43289be6d9c4cbc652685c4abf93cd15a
ingvord <igor.khokhriakov@rcsb.org>
2023-05-23 13:43:01 -0700

    Progress

--- src/test/java/org/rcsb/idmapper/client/IdMapperHttpClientTest.java
+++ src/test/java/org/rcsb/idmapper/client/IdMapperHttpClientTest.java
@@ -6,11 +6,14 @@ import org.junit.jupiter.api.Test;
 import org.rcsb.common.constants.ContentType;
 import org.rcsb.idmapper.input.Input;
 import org.rcsb.idmapper.input.TranslateInput;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Sinks;
 
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.time.Duration;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 
 class IdMapperHttpClientTest {
@@ -23,18 +26,28 @@ class IdMapperHttpClientTest {
         input.to =  Input.Type.polymer_entity;
         input.content_type = List.of(ContentType.experimental);
 
-        var client = HttpClient.newBuilder()
+        var httpClient = HttpClient.newBuilder()
                 .version(HttpClient.Version.HTTP_2)
                 .connectTimeout(Duration.ofSeconds(3))
                 .build();
 
-        var result = new IdMapperHttpClient(client, URI.create("http://localhost:8080"), new JsonMapper().create()).doTranslate(
-                input
-        );
-        var actual = result.block();
-        System.out.println(MoreObjects.toStringHelper(actual)
-                .add("result", actual.results)
-                .toString());
+        var client = new IdMapperHttpClient(httpClient, URI.create("http://localhost:8080"), new JsonMapper().create());
+
+
+        Flux.range(1,1000)
+                .elapsed()
+                .subscribe(tuple -> {
+                            System.out.println("Elapsed: " + tuple.getT1());
+                    var result = client.doTranslate(
+                            input
+                    );
+                    var actual = result.block();
+                    System.out.println(MoreObjects.toStringHelper(actual)
+                            .add("result", actual.results)
+                            .toString());
+                }
+
+                );
     }
 
     @Test
