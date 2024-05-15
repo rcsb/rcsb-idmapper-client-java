0fc70c076e33293396fce312b9ab3f88251d27fb
valasatava <yana.valasatava@gmail.com>
2024-05-15 15:41:45 -0700

    minor wording update

--- src/main/java/org/rcsb/idmapper/client/HttpTransport.java
+++ src/main/java/org/rcsb/idmapper/client/HttpTransport.java
@@ -26,7 +26,7 @@ public abstract class HttpTransport<T extends Output<?>> implements Transport<T>
         var request = HttpRequest.newBuilder(uri)
                 .POST(HttpRequest.BodyPublishers.ofString(jsonMapper.writeValueAsString(input)))//TODO this is possibly super un-optimal
                 .setHeader("Content-Type", "application/json")
-                .setHeader("User-Agent", "IdMapper Java Client")//TODO version
+                .setHeader("User-Agent", "ID-Mapper Java Client")//TODO version
                 .build();
 
         return handleResponse(request);
